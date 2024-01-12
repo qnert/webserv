87b5292ef4bf94d9e88a617203cb9c62a939ef3b
Nicolas Jantsch <njantsch@1-A-14.42heilbronn.de>
2024-01-12 17:12:50 +0100

    files added

---
+++ includes/Server.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <cstdio>
+#include <string>
+#include <cstdlib>
+
+class Server
+{
+private:
+
+public:
+  Server();
+  ~Server();
+};
