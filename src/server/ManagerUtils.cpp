af1b1f9bbf73b50e00ab92a5a0a459fcbd645b48
Rouven Nauke <rnauke@1-A-6.42heilbronn.de>
2024-02-22 18:56:47 +0100

    resolves multiple server names and fixed server name matching

--- src/server/ManagerUtils.cpp
+++ src/server/ManagerUtils.cpp
@@ -6,7 +6,7 @@
 /*   By: rnauke <rnauke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 14:36:32 by njantsch          #+#    #+#             */
-/*   Updated: 2024/02/22 16:22:19 by rnauke           ###   ########.fr       */
+/*   Updated: 2024/02/22 18:55:47 by rnauke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,25 +44,30 @@ bool ServerManager::isServerSocket(int fd)
 
 void ServerManager::matchRequestToServer(size_t index)
 {
-	// implement that if theres no server name provided any connection with the correct port gets accepted
 	std::string hostname =  this->_clientDetails[index].getMapValue("Host");
+	std::string token;
 	if (hostname.find(':') == std::string::npos)
 		hostname.append(":80");
 	for (std::vector<Server>::iterator i = this->_servers.begin(); i < this->_servers.end(); ++i)
 	{
-		if ((*i).getServername() == hostname.substr(0, hostname.find(':')) && (*i).getPort() == hostname.substr(hostname.find(':')+1))
+		std::istringstream names((*i).getServername());
+		while (names >> token)
 		{
-			this->_currentServer = *i;
-			std::cout << (*i).getServername() << std::endl;
-			return;
+			std::cout << "token: " << token << std::endl;
+			if (token == hostname.substr(0, hostname.find(':')) && (*i).getPort() == hostname.substr(hostname.find(':')+1))
+			{
+				this->_currentServer = *i;
+				_clientDetails[index].setConStatus(KEEPALIVE);
+				return;
+			}
+			else if ((*i).getPort() == hostname.substr(hostname.find(':') + 1))
+			{
+				this->_currentServer = *(_servers.begin());
+				_clientDetails[index].setConStatus(KEEPALIVE);
+				return;
+			}
+			else
+				_clientDetails[index].setConStatus(CLOSE);
 		}
-		else if ((*i).getPort() == hostname.substr(hostname.find(':') + 1))
-		{
-			this->_currentServer = *(_servers.begin());
-			std::cout << (*_servers.begin()).getServername() << std::endl;
-			return;
-		}
-		else
-			_clientDetails[index].setConStatus(CLOSE);
 	}
 }
